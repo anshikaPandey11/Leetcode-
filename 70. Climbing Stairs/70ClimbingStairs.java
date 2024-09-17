68aab53aef16d0991af9b32362a3c87f899eb481
Anshika Pandey <anskikaallen0203@gmail.com>
2024-09-18 00:08:28 +0530

    Math,Dynamic Programming,Memoization

---
+++ 70. Climbing Stairs/70ClimbingStairs.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int climbStairs(int n) {
+        int w0=1;
+        int w1=1;
+        for(int i=2;i<=n;i++){
+           int tw=w0+w1;
+            w0=w1;
+            w1=tw;
+        }
+        return w1;
+    }
+}
