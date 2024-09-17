18e0649f3119a373d9425c1079de7b01da7f779c
Anshika Pandey <anskikaallen0203@gmail.com>
2024-09-17 23:47:46 +0530

    Array,Binary Search

---
+++ 35. Search Insert Position/35SearchInsertPosition.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int first = 0;
+        int last = nums.length - 1;
+        while (first <= last) {
+            int mid = first + (last - first) / 2;
+            if (target > nums[mid]) {
+                first = mid + 1;
+            } else if (target < nums[mid]) {
+                last = mid - 1;
+            } else {
+                return mid;
+            }
+        }
+        return first;
+    }
+}
