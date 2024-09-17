7be6ebcd68e8618177312bc80615326209a946a0
Anshika Pandey <anskikaallen0203@gmail.com>
2024-09-18 00:21:29 +0530

    Array,Greedy

---
+++ 330. Patching Array/330PatchingArray.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int minPatches(int[] nums, int n) {
+        long missing=1;
+        int patch=0;
+        int i=0;
+        while(missing<=n){
+            if(i<nums.length&& nums[i]<= missing){
+               missing+=nums[i];
+               i++;
+            }else{
+                 missing+=missing;
+                 patch++;
+            }
+        }
+       return patch;
+    }   
+}
